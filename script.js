229da92506fadb1d81eb56226afe80b2709fe904
mudkippyy <ninjamorn@gmail.com>
2023-01-18 21:37:38 -0500

    commit

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log("s")
+
+function clearResult(){
+    document.getElementById("result").value = "";
+    console.log("ss")
+}
+
+function display(value){
+    length = document.getElementById("result").value.length;
+    if(length == 10){
+        return;
+    }
+
+    document.getElementById("result").value += value;
+}
+
+function calc(){
+    var x = document.getElementById("result").value;
+    try{
+        var p = eval(x);
+        
+        document.getElementById("result").value = p;
+        
+    }
+    catch(err){
+        document.getElementById("result").value = "ERROR";
+    }
+}
+
